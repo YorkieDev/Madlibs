0ef6c51b4936c9f6a026c73b8cd527454ba8d889
Iain <42919623+YorkieDev@users.noreply.github.com>
2023-10-08 15:09:30 +0100

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,12 @@
+#Madlibs
+
+story_template = "The {noun} {adjective} {verb} over the {place}."
+
+noun = input("Please Enter a Noun: ")
+adjective = input("Please Enter an Adjective: ")
+verb = input("Please Enter a Verb: ")
+place = input("Please Enter a Place ")
+
+completed_story = f"The {noun} {adjective} {verb} over the {place}."
+print("Here is your madlibs story: ")
+print(completed_story)
